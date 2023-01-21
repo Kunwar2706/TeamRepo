3e62c34b5300f98f3a5b8eaaa0f40d58b02629e7
gakhi <gakhi@DESKTOP-DB9ED7V>
2023-01-21 12:17:01 +0530

    Akhilesh commit

---
+++ src/test/java/maven_practice/EditModuleTest.java
@@ -0,0 +1,12 @@
+package maven_practice;
+
+import org.testng.annotations.Test;
+
+public class EditModuleTest {
+	@Test
+	public void editModule()
+	{
+		System.out.println("----Edit Successfully____");
+	}
+
+}
